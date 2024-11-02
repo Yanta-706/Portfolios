82da1235582ab0b30718eca6fd181a152ac7f40c
Evan Huang <evan.huang000@proton.me>
2024-11-02 10:15:32 +0800

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,4 @@
 import { MetadataRoute } from "next";
-import { fetchBlogs } from "./utils/fetchWordpress";
 import { TPost } from "./utils/types";
 
 export async function generateSitemaps() {
@@ -9,18 +8,11 @@ export async function generateSitemaps() {
 }
 
 export default async function sitemap({ id }: { id: number }): Promise<MetadataRoute.Sitemap> {
-    const posts = await fetchBlogs();
 
     return [
         {
             url: "https://alvinchang.dev",
             lastModified: new Date(),
         },
-
-        ...posts.map((post: TPost) => ({
-            url: `https://alvinchang.dev/blogs/${post.slug}`,
-            lastModified: new Date(post.modified_gmt),
-            priority: 0.8,
-        }))
     ];
 }
