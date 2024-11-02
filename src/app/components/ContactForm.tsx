684e6ec02d75d7774276380c839860a2778be4a2
Evan Huang <evan.huang000@proton.me>
2024-11-02 10:26:19 +0800

    Update ContactForm.tsx

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -3,9 +3,7 @@
 import MotionDiv from "./MotionDiv";
 import { slideInFromRight } from "../utils/motion";
 import { Send } from "lucide-react";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import * as z from "zod";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
@@ -17,74 +15,22 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import SectionLabel from "./SectionLabel";
-import { sendMail } from "../utils/sendMail";
-import { toast } from "sonner";
-
-const formSchema = z.object({
-    name: z.string().min(2, {
-        message: "Name must be at least 2 characters.",
-    }),
-    email: z.string().email({
-        message: "Please enter a valid email address.",
-    }),
-    message: z.string().min(10, {
-        message: "Message must be at least 10 characters.",
-    }),
-});
 
 export default function ContactForm() {
     const [isLoading, setIsLoading] = useState(false);
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm({
         defaultValues: {
-            name: "",
-            email: "",
             message: "",
         },
     });
 
-    let isMobile = false;
-    if (typeof window !== "undefined") {
-        isMobile = window.matchMedia("(max-width: 1024px)").matches;
-    }
-
-    async function onSubmit(values: z.infer<typeof formSchema>) {
+    function onSubmit(values) {
         setIsLoading(true);
-        const response = await sendMail(
-            values.name,
-            values.email,
-            values.message
-        );
+        const mailtoLink = `mailto:evan.huang000@proton.me?subject=Message from Contact Form&body=${encodeURIComponent(values.message)}`;
+        window.location.href = mailtoLink;
         setIsLoading(false);
-
-            if (response && response.success) {
-            toast.success(response.message, {
-                description: "Your message has been sent. I will get back to you as soon as possible.",
-                duration: 5000,
-                position: isMobile ? "top-center" : "bottom-right",
-                classNames: {
-                    icon: "text-green-500",
-                    toast: "bg-background text-slate-100 border-slate-900",
-                    description: "text-slate-350",
-                    
-                }
-            });
-        } else if (response && !response.success) {
-            toast.error(response.message, {
-                description: "Please try again later.",
-                duration: 5000,
-                position: isMobile ? "top-center" : "bottom-right",
-                classNames: {
-                    icon: "text-red-600",
-                    toast: "bg-background text-slate-100 border-slate-900",
-                    description: "text-slate-350",
-                    
-                }
-            });
-        }
         form.reset();
     }
 
@@ -102,44 +48,6 @@ export default function ContactForm() {
                         onSubmit={form.handleSubmit(onSubmit)}
                         className="grid grid-cols-2 gap-4"
                     >
-                        <FormField
-                            control={form.control}
-                            name="name"
-                            render={({ field }) => (
-                                <FormItem className="col-span-2 md:col-span-1">
-                                    <FormLabel className="text-slate-100 text-xs">
-                                        Name
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            placeholder="Alvin Chang"
-                                            {...field}
-                                            className="bg-[rgba(255,255,255,0.01)] border-[rgba(255,255,255,0.1)] text-white text-xs p-2 rounded-sm focus:ring-orange-500"
-                                        />
-                                    </FormControl>
-                                    <FormMessage className="text-xs" />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="email"
-                            render={({ field }) => (
-                                <FormItem className="col-span-2 md:col-span-1">
-                                    <FormLabel className="text-slate-100 text-xs">
-                                        Email
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            placeholder="alvin@studioaurora.io"
-                                            {...field}
-                                            className="bg-[rgba(255,255,255,0.01)] border-[rgba(255,255,255,0.1)] text-white text-xs p-2 rounded-sm focus:ring-orange-500"
-                                        />
-                                    </FormControl>
-                                    <FormMessage className="text-xs" />
-                                </FormItem>
-                            )}
-                        />
                         <FormField
                             control={form.control}
                             name="message"
