e8c2c9d720e7a95d2c329655b1ea94c680d39d6a
Evan Huang <evan.huang000@proton.me>
2024-11-08 19:37:32 +0800

    Update HeroTitle.tsx

--- src/app/components/HeroTitle.tsx
+++ src/app/components/HeroTitle.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { motion } from "framer-motion";
 import { slideInFromLeft, slideInFromTop } from "../utils/motion";
 import Image from "next/image";
+import MotionTag from "./MotionTag";
 
 export default function HeroTitle() {
     let isMobile = false;
@@ -12,9 +12,10 @@ export default function HeroTitle() {
 
     return (
         <div>
-            <motion.div
-                variants={isMobile ? slideInFromTop(0.5) : slideInFromLeft(0.5)}
-                initial="hidden"
+            <MotionTag
+                tag="div"
+                variants={slideInFromLeft(0.5)}
+                initial={isMobile ? 'visible' : "hidden"}
                 animate="visible"
                 className="text-lg sm:text-xl md:text-2xl font-medium flex items-center w-full justify-center lg:justify-start"
             >
@@ -25,26 +26,27 @@ export default function HeroTitle() {
                     width={28}
                     height={28}
                 />
-                <h1>Hi, I'm Evan! 👋</h1>
-            </motion.div>
-            <motion.h1
-                variants={isMobile ? slideInFromTop(0.6) : slideInFromLeft(0.6)}
-                initial="hidden"
+                <h1>Hey there, I'm Alvin! 👋</h1>
+            </MotionTag>
+            <MotionTag
+                tag="h1"
+                variants={slideInFromLeft(0.6)}
+                initial={isMobile ? 'visible' : "hidden"}
                 animate="visible"
-                className="text-4xl sm:text-5xl lg:text-5xl 2xl:text-6xl font-bold text-center lg:text-start"
+                className="text-4xl sm:text-5xl lg:text-[2.75rem] xl:text-5xl 1640:text-[3.25rem] font-bold text-center lg:text-start"
             >
-                I'm a
-                <span className="gradient-accent"> Front-End  </span>
-
-            </motion.h1>
-            <motion.h1
+                Delivering
+                <span className="gradient-accent"> the best </span>
+            </MotionTag>
+            <MotionTag
+                tag="h1"
                 variants={isMobile ? slideInFromTop(0.7) : slideInFromLeft(0.7)}
                 initial="hidden"
                 animate="visible"
-                className="text-4xl sm:text-5xl lg:text-5xl 2xl:text-6xl font-bold text-center lg:text-start mb-2"
+                className="text-4xl sm:text-5xl lg:text-[2.75rem] xl:text-5xl 1640:text-[3.25rem] font-bold text-center lg:text-start mb-2"
             >
-                Developer
-            </motion.h1>
+                in design and code.
+            </MotionTag>
         </div>
     );
 }
